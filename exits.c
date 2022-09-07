c48df054b91ed5636dd9771abff49acc098edc26
saheedsalawu <106780350+saheedsalawu@users.noreply.github.com>
2022-09-07 13:17:54 -0900

    Update exits.c

--- exits.c
+++ exits.c
@@ -1,28 +1,74 @@
 #include "shell.h"
 
 /**
- * get_builtin - builtin that pais the command in the arg
- * @cmd: command
- * Return: function pointer of the builtin command
+ **_strncpy - copies a string
+ *@dest: the destination string to be copied to
+ *@src: the source string
+ *@n: the amount of characters to be copied
+ *Return: the concatenated string
  */
-int (*get_builtin(char *cmd))(data_shell *)
+char *_strncpy(char *dest, char *src, int n)
 {
-	builtin_t builtin[] = {
-		{ "env", _env },
-		{ "exit", exit_shell },
-		{ "setenv", _setenv },
-		{ "unsetenv", _unsetenv },
-		{ "cd", cd_shell },
-		{ "help", get_help },
-		{ NULL, NULL }
-	};
-	int j;
+	int i, j;
+	char *s = dest;
 
-	for (j = 0; builtin[i].name; j++)
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
 	{
-		if (_strcmp(builtin[i].name, cmd) == 0)
-			break;
+		dest[i] = src[i];
+		i++;
 	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
 
-	return (builtin[j].f);
+/**
+ **_strncat - concatenates two strings
+ *@dest: the first string
+ *@src: the second string
+ *@n: the amount of bytes to be maximally used
+ *Return: the concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: (s) a pointer to the memory area s
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
 }
